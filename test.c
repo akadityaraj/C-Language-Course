e7335b9903423c65e849619bec3b7205836baa6e
AK ADITYA RAJ <AKADITYAKMRSB@OUTLOOK.COM>
2026-08-08 08:14:30 +0530

    Solve some Promblem

--- test.c
+++ test.c
@@ -1,5 +1,34 @@
-#include<stdio.h>
+// Sum of two numbers Program
+/* #include<stdio.h>
+
 int main() {
-    printf("Hello, World!\n");
-    
+    int number1 , number2, sum;
+    printf("Enter First Number: ");
+    scanf("%d", &number1);
+    printf("Enter Second Number: ");
+    scanf("%d", &number2);
+    sum = number1 + number2;
+    printf("Sum is %d", sum);
+    return 0;
+}*/
+/*
+// Area of square Program
+#include <stdio.h>
+int main(){
+    int side;
+    printf("Enter the side of square: ");
+    scanf("%d", &side);
+    printf("Area of square is: %d", side * side);
+    return 0;
+}
+*/
+// Area of circle Program
+
+#include<stdio.h>
+int main(){
+    float radius;
+    printf("Enter the Radius of Circle: ");
+    scanf("%f", &radius);
+    printf("Area is : %f", 3.14 * radius * radius );
+    return 0;
 }
