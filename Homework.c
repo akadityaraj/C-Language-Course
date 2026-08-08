b05f12fe4ab0f549ae4c3355498206254cd44873
AK ADITYA RAJ <AKADITYAKMRSB@OUTLOOK.COM>
2026-08-08 17:08:57 +0530

    Some Practice solve

---
+++ Homework.c
@@ -0,0 +1,13 @@
+/*                                 
+#include<stdio.h>
+int main () {
+    int a , b , c;
+    printf("Enter Length: ");
+    scanf("%d", &a );
+    printf("Enter Breadth: ");
+    scanf("%d", &b );
+    c = a + b ;
+    printf("Perimeter of Rectangle %d\n",  2 * c);
+    return 0;
+}
+*/
