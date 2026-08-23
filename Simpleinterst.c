2e81bbc923a3544e6bd10a0e80bfd32340b63a68
AK ADITYA RAJ <AKADITYAKMRSB@OUTLOOK.COM>
2026-08-23 09:48:54 +0530

    simple interest program

---
+++ Simpleinterst.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float p,r,t,si;
+    printf("enter your P: ");
+    scanf("%f",&p);
+    printf("enter your R: ");
+    scanf("%f",&r);
+    printf("enter your T: ");
+    scanf("%f",&t);
+    si=(p*r*t)/100;
+    printf("%f",si);
+    
+}
