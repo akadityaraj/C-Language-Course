407d392d853155d71d432e03e35ee18cbd452bf9
AK ADITYA RAJ <AKADITYAKMRSB@OUTLOOK.COM>
2026-08-25 20:09:59 +0530

    Add support for a fourth number
    Declare and read a fourth integer (d) in greatestfinder

--- greatestfinder.c
+++ greatestfinder.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 int main()
 {
-    int a, b, c;
+    int a, b, c, d;
     printf("Enter Your First Number:");
     scanf("%d", &a);
     printf("Enter Your Secound Number:");
     scanf("%d", &b);
     printf("Enter Your Third Number:");
     scanf("%d", &c);
-    if (a > b && a > c)
+    printf("Enter Your Fourth Number:");
+    scanf("%d", &d);
+    if (a > b && a > c && a > d)
     {
         printf("%d is Greater Number", a);
     }
-    if (b > a && b > c)
+    if (b > a && b > c && b > d)
     {
         printf("%d is Greater Number", b);
     }
-    if (c > a && c > b)
+    if (c > a && c > b && c > d)
     {
         printf("%d is Greater Number", c);
+    }
+    if (d > a && d > b && d > c)
+    {
+        printf("%d is Greater Number", d);
     }
     return 0;
 }
